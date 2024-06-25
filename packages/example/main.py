c536bdc26e6f30ab990fd36d5e950378b11a3597
LOAD <122114788+load-dev@users.noreply.github.com>
2024-06-25 11:19:03 +0000

    Create of Examples

---
+++ packages/example/main.py
@@ -0,0 +1,6 @@
+# in this file needs to be written what it will be doing. Basicly the whole logic. It can be in .exe or in .py
+import colorama
+from colorama import Fore
+
+print(f"{Fore.RED}Hi, {Fore.GREEN}how are yall?")
+# end of program is where the file ends.
